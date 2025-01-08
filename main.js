327c148b5df38b03f088f447718603addaca4f1d
WataruOhkura <wohkura@nexseed.net>
2025-01-08 10:09:16 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+{
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector(".nav");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("open");
+        nav.classList.toggle("open");
+    });
+}
